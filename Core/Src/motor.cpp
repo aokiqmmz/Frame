dca00d74e8bdc2d7f2005a0572e93c8de5e0df9f
aokiqmmz <ly_jin@sjtu.edu.cn>
2024-12-06 15:10:42 +0800

    imu

--- Core/Src/motor.cpp
+++ Core/Src/motor.cpp
@@ -5,19 +5,19 @@
 #include "motor.h"
 #include <cstdint>
 
-// 基类构造函数
+
 Motor::Motor(float ratio)
     : ratio_(ratio), delta_angle_(0.0f), angle_(0.0f), last_ecd_angle_(0.0f),
       delta_ecd_angle_(0.0f), rotate_speed_(0.0f), current_(0.0f), temp_(0.0f) {}
 
-// 线性映射函数
+
 float Motor::linearMapping(int value, int in_min, int in_max, float out_min, float out_max) {
     float out = (value - in_min) *1.0/ (in_max - in_min) * (out_max - out_min) + out_min;
     return out;
 }
 
-// 更新电机状态（虚函数，子类可以重写）
-//这是基于GM6020手册的
+
+//基于GM6020
 void Motor::updateMotorStatus(uint8_t rx_data[8]) {
     int16_t ecd_angle_raw = (rx_data[0] << 8 | rx_data[1]);
     float ecd_angle_ = linearMapping(ecd_angle_raw, 0, 8191, 0.0, 360.0);
@@ -36,7 +36,7 @@ void Motor::updateMotorStatus(uint8_t rx_data[8]) {
 
 
 
-// 子类构造函数
+
 M3508Motor::M3508Motor() : Motor(3591.0f / 187.0f) {}
 void M3508Motor::updateMotorStatus(uint8_t rx_data[8]) {
     Motor::updateMotorStatus(rx_data);
@@ -53,6 +53,5 @@ void GM6020Motor::updateMotorStatus(uint8_t rx_data[8]) {
 
 }
 
-// 实例化电机对象
 GM6020Motor pit_motor;
 GM6020Motor yaw_motor;
